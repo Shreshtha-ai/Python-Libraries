9592610dc38d7d59756e78073f3d662056b2c8b0
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-04-01 23:32:58 +0530

    created special arrays

--- arraybasic.py
+++ arraybasic.py
@@ -17,10 +17,27 @@ print("Python list multiplication", py_list*3)
 numpy_array = np.array([1,2,3])
 print("Numpy array multiplication", numpy_array*3) # element wise multiplication 
 
-start = time.time()
-py_list = [i * 2 for i in range(100000000)]
-print("\n Python list multiplication time :", time.time() - start)
+# start = time.time()
+# py_list = [i * 2 for i in range(100000000)]
+# print("\n Python list multiplication time :", time.time() - start)
 
-start = time.time()
-numpy_array = np.arange(100000000) * 2
-print("\n Numpy array multiplication time :", time.time() - start)
+# start = time.time()
+# numpy_array = np.arange(100000000) * 2
+# print("\n Numpy array multiplication time :", time.time() - start)
+
+#creating arrays 
+
+zeros = np.zeros((3,4))
+print("Zeros array:",zeros)
+
+ones = np.ones((3,4))
+print("Ones array:",ones)
+
+full = np.full((3,4),7)
+print("Full array:",full)
+
+random = np.random.random((2,2))
+print("Random array:",random)
+
+sequence = np.arange(1,10,2)
+print("Sequence array:",sequence)
