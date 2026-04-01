d82b66783af47a07cec108afbc45dfc0bf3afbe1
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-04-02 00:07:55 +0530

    added basic of matrix, tensor and some properties of arrays

--- arraybasic.py
+++ arraybasic.py
@@ -41,3 +41,46 @@ print("Random array:",random)
 
 sequence = np.arange(1,10,2)
 print("Sequence array:",sequence)
+
+# vector, matrix,tensor
+
+vector = np.array([1,2,3])
+print("Vector:",vector)
+
+matrix = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+print("Matrix:",matrix)
+
+tensor = np.array([[[1,2,3],[4,5,6]],
+                   [[7,8,9],[10,11,12]]])
+print("Tensor:",tensor)
+
+# ARRAY PROPERTIES
+
+arr = np.array([[1,2,3],
+                [4,5,6]])
+print("Shape:",arr.shape) # no of rows and columns
+print("Dimensions:",arr.ndim) # no of dimensions
+print("Size:",arr.size) # no of elements
+print("Data type:",arr.dtype) # data type of elements
+
+
+#Array Reshaping
+
+arr1 = np.array([1,2,3,4,5,6])
+print("Original array:",arr1)
+reshaped = arr1.reshape(2,3)
+print("Reshaped array:",reshaped)
+
+flattened = reshaped.flatten() # returns a copy of the original array
+print("Flattened array:",flattened)
+
+raveled = reshaped.ravel() # returns a view of the original array
+print("Raveled array:",raveled)
+
+transpose = reshaped.T
+print("Transpose array:",transpose)
+
+
+                             
