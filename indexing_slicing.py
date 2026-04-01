f43dff4787b6fb6dc3ae93aab6387d12de1da81a
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-04-02 00:18:40 +0530

    added indexing

---
+++ indexing_slicing.py
@@ -0,0 +1,16 @@
+#indexing 
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print("Basic Slicing", arr[2:7])
+print("With Step", arr[1:8:2])
+print("Negative Indexing",arr[-3])
+
+
+arr_2d = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+print("Specific element", arr_2d[1, 2])
+print("Entire row: ", arr_2d[1])
+print("Entire column:", arr_2d[:,2])
+
+#sorting
